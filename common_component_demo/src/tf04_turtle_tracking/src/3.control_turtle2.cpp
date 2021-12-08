25fc5df57daea9274fe0ad7c91384063cfe3877d
peterhuang <qsmy122011@gmail.com>
2021-12-08 14:55:54 +0000

    demo:trakcing[Done]: tracking works

--- common_component_demo/src/tf04_turtle_tracking/src/3.control_turtle2.cpp
+++ common_component_demo/src/tf04_turtle_tracking/src/3.control_turtle2.cpp
@@ -3,6 +3,7 @@
 #include "tf2_ros/buffer.h"
 #include "geometry_msgs/PointStamped.h"
 #include "tf2_geometry_msgs/tf2_geometry_msgs.h"
+#include "geometry_msgs/Twist.h"
 
 
 /* 
@@ -25,6 +26,11 @@ int main(int argc, char* argv[])
     //3,创建订阅对象;
     tf2_ros::Buffer buffer;
     tf2_ros::TransformListener sub(buffer);
+
+
+    //(A) 创建发布对象
+    ros::Publisher pub = nh.advertise<geometry_msgs::Twist>("/turtle2/cmd_vel", 100);
+
     
     //4,编写解析逻辑;
 
@@ -33,15 +39,28 @@ int main(int argc, char* argv[])
         // 核心
         try{
             // 需求1：换算出turtle1(敌人) 相对于 turtle2(导弹)的关系
-            geometry_msgs::TransformStamped tfs = buffer.lookupTransform("turtle2","turtle1",ros::Time(0)); // target is fixed, is the referenced one.
-            ROS_INFO("TRANSFORM:");
-            ROS_INFO("base ID=%s",tfs.header.frame_id.c_str()); // turtle2坐标系
-            ROS_INFO("child ID=%s",tfs.child_frame_id.c_str()); // turtle1
-            ROS_INFO("trt1 away from trt2 =%.2f,y=%.2f,z=%.2f",
-                    tfs.transform.translation.x,
-                    tfs.transform.translation.y,
-                    tfs.transform.translation.z
-                    );
+            geometry_msgs::TransformStamped tfs = buffer.lookupTransform("turtle2","turtle1",ros::Time(0.0)); // target is fixed, is the referenced one.
+            // ROS_INFO("TRANSFORM:");
+            // ROS_INFO("base ID=%s",tfs.header.frame_id.c_str()); // turtle2坐标系
+            // ROS_INFO("child ID=%s",tfs.child_frame_id.c_str()); // turtle1
+            // ROS_INFO("trt1 away from trt2 =%.2f,y=%.2f,z=%.2f",
+            //         tfs.transform.translation.x,
+            //         tfs.transform.translation.y,
+            //         tfs.transform.translation.z
+            //         );
+            //(B) 根据相对计算，组织速度消息
+            /* 
+                组织速度，只需要设置线速度的 x 与角速度 的 z
+                x = 系数 * 开方（y^2 + x^2）
+                z = 系数 * 反正切（对边，邻边）
+            
+             */
+            geometry_msgs::Twist twist;
+            twist.linear.x = 0.5 * sqrt(pow(tfs.transform.translation.x,2) + pow(tfs.transform.translation.y,2));
+            twist.angular.z = 4 * atan2(tfs.transform.translation.y,tfs.transform.translation.x);
+
+            //(C) 发布
+            pub.publish(twist);
 
         }
         catch(const std::exception& e){
